5967f7524b47a40ad40509da075ad78a26cd3b3e
maomao <2892798998@qq.com>
2021-08-23 08:24:09 +0800

    algorithm(two_sum): add algorithm of two_sum

--- array/two_sum_test.go
+++ array/two_sum_test.go
@@ -6,13 +6,17 @@ import (
 )
 
 func TestMain(m *testing.M) {
-	fmt.Println(" my test is beginning... ")
-	// m.run() -> trigger test execute
+	fmt.Println(" twoSum test is beginning... ")
 	m.Run()
 }
 
 func TestTwoSum1(t *testing.T) {
-	// t.run(name, func)
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	result := twoSum(nums, target)
+	if len(result) != 2 {
+		t.Fail()
+	}
 }
 
 func BenchmarkTwoSum(b *testing.B) {
